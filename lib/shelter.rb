479f38b872209e66f13316bd6562ae7bd7132a9b
Kyle Ledin <Kyle.Ledin85@gmail.com>
2022-10-12 10:28:07 -0500

    Have I1 complete

--- lib/shelter.rb
+++ lib/shelter.rb
@@ -1,2 +1,18 @@
 class Shelter
+
+attr_reader :name, :capacity, :pet
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @pet = pet
+  end
+
+  def pets
+    @capacity = []
+  end
+
+  def add_pet
+    
+  end
 end
